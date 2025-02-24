0f6b94bfba03da5c03e52a768435e1c34910f207
hayden-sanders <hayden.sanders@student.tamdistrict.org>
2025-02-24 22:14:14 +0000

    tragic

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,72 +1,125 @@
-import { Scene } from 'phaser';
+import { Physics, Scene } from 'phaser';
 
-export class Game extends Scene
-{
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    msg_text: Phaser.GameObjects.Text;
 
-    constructor ()
-    {
+    constructor() {
         super('Game');
     }
-    preload () {
+    preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 
+        this.load.spritesheet('dude',
             'assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
         );
-     }
-    
-     platforms: any;
-     player: any;
+    }
 
-    create () {
-    this.add.image(505, 390, 'sky')
+    platforms: any;
+    player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    cursors: any;
+    stars: any;
+    collectStar(player: any, star: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody){
+        star.disableBody(true,true);
+    }
 
-    this.platforms = this.physics.add.staticGroup();
+    create() {
+        this.cursors = this.input.keyboard!.createCursorKeys();
 
-    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.add.image(400, 300, 'sky')
 
-    this.platforms.create(600, 400, 'ground');
-    this.platforms.create(50, 250, 'ground');
-    this.platforms.create(750, 220, 'ground');
+        this.platforms = this.physics.add.staticGroup();
 
-        
-    this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-    this. player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-    
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    
-    this.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-    
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+
+
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        //this. player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+
+
+        this.player.body.setGravityY(300)
+
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+
+        this.stars.children.iterate(function (child) {
+
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+        });
+
+    }
+
+
+    update() {
+
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, undefined, this);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.physics.add.collider(this.stars, this.platforms);
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-600);
+        }
+
+
+
+    }
+
 
 }
 
-
-    update(){
-    
-}
-
-
-}
